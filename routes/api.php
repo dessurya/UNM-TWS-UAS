be083109668cf5fb7d02033e5ce0e9c42f24375c
Adam Surya Des <desadamsury93@gmail.com>
2022-06-04 13:48:19 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\InboxController;
+// use App\Http\Controllers\InboxController;
 
 /*
 |--------------------------------------------------------------------------
