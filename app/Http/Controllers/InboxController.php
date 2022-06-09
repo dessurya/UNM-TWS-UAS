af09d47a800a4de64f2721b6a55d73b999f38b74
Adam Surya Des <desadamsury93@gmail.com>
2022-06-09 21:56:30 +0700

    .

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -43,8 +43,8 @@ class InboxController extends Controller
             'endpoint' => [
                 'list' => ['url'=>route('inbox-list'), 'method' => 'GET'],
                 'store' => ['url'=>route('inbox-store'), 'method' => 'POST'],
-                'update' => ['url'=>route('inbox-update',['id'=>1]), 'method' => 'PUT'],
-                'open' => ['url'=>route('inbox-open',['id'=>1]), 'method' => 'GET'],
+                'update' => ['url'=>route('inbox-update',null), 'method' => 'PUT'],
+                'open' => ['url'=>route('inbox-open',null), 'method' => 'GET'],
                 'delete' => ['url'=>route('inbox-delete'), 'method' => 'DELETE'],
             ]
         ];
